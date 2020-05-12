743b54d5b358a7f87ffbfc5666abf9c9cff0819a
webercost4 <61632304+webercost4@users.noreply.github.com>
2020-05-11 21:15:46 -0300

    Add files via upload
    Forma de ultilizar - [Condição sql][bindValues] 
    Cada " ? " Representa sua posição nos bindValues EX:  (?, ?) -- Neste caso o primeiro " ? " recebe o valor 1 e o segundo recebe valor 2.
    
    
    Exemplo SELECT -- $new->Crud("SELECT * FROM test WHERE id = ? ", [ 1 ]);
    
    Exemplo INSERT --  $new->Crud("INSERT INTO test (nome, sobre) VALUES (?, ?)", [ ' Nome ', 'Sobre Nome' ]);
    
    Exemplo UPDATE -- $new->Crud("UPDATE test SET nome = ?  WHERE id = ?", [ ' Testando ',  2 ]);
    
    Exemplo DELETE --  $new->Crud("DELETE FROM test WHERE id = ?", [ 2 ]);

---
+++ Crud.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Crud
+ *
+ * @author weber
+ */
+
+class CrudPHP {
+    
+    public function Crud($sql, $bind){
+        
+        $this->ConnectDB();
+        
+        $this->stmt = $this->conexao->prepare("$sql");
+
+        foreach ($bind as $bindV => $key) :
+            $this->stmt->bindValue($bindV + 1, $key);
+        endforeach;
+
+        $this->stmt->execute();
+
+    }
+    
+}
+
+$new = new CrudPHP;
+
